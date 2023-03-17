a94f5ed79d96d58762586569a3237af60a0c2667
jasroop-singh <113493289+jasroop-singh@users.noreply.github.com>
2023-03-17 15:41:37 -0700

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,41 @@
+
+public class Customer {
+	
+	private int customerID;
+	private String customerName;
+	private String customerAddress;
+	private String paymentInfo;
+	private int phoneNumber;
+	
+	public int getCustomerID() {
+		return customerID;
+	}
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getCustomerAddress() {
+		return customerAddress;
+	}
+	public void setCustomerAddress(String customerAddress) {
+		this.customerAddress = customerAddress;
+	}
+	public String getPaymentInfo() {
+		return paymentInfo;
+	}
+	public void setPaymentInfo(String paymentInfo) {
+		this.paymentInfo = paymentInfo;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+}
