b1605ae6881057dd14e9ba6d6d038d07fbbdb8f0
jasroop-singh <113493289+jasroop-singh@users.noreply.github.com>
2023-03-17 15:42:14 -0700

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,34 @@
+
+public class Driver {
+	
+	private String driverName;
+	private int driverID;
+	private String availability;
+	private int phoneNumber;
+	
+	public String getDriverName() {
+		return driverName;
+	}
+	public void setDriverName(String driverName) {
+		this.driverName = driverName;
+	}
+	public int getDriverID() {
+		return driverID;
+	}
+	public void setDriverID(int driverID) {
+		this.driverID = driverID;
+	}
+	public String getAvailability() {
+		return availability;
+	}
+	public void setAvailability(String availability) {
+		this.availability = availability;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
