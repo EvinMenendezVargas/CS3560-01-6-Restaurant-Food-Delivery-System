ba8c003407249e03090e277ed03e1c59e77261fe
jasroop-singh <113493289+jasroop-singh@users.noreply.github.com>
2023-03-17 15:42:48 -0700

    Create Menu.java

---
+++ Menu.java
@@ -0,0 +1,27 @@
+
+public class Menu {
+	
+	private String foodName;
+	private int foodItemID;
+	private double foodPrice;
+	
+	public String getFoodName() {
+		return foodName;
+	}
+	public void setFoodName(String foodName) {
+		this.foodName = foodName;
+	}
+	public int getFoodItemID() {
+		return foodItemID;
+	}
+	public void setFoodItemID(int foodItemID) {
+		this.foodItemID = foodItemID;
+	}
+	public double getFoodPrice() {
+		return foodPrice;
+	}
+	public void setFoodPrice(double foodPrice) {
+		this.foodPrice = foodPrice;
+	}
+	
+}
