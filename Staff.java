81eab114618689239aa69762bd41e6c32427e4e7
jasroop-singh <113493289+jasroop-singh@users.noreply.github.com>
2023-03-17 15:43:17 -0700

    Create Staff.java

---
+++ Staff.java
@@ -0,0 +1,34 @@
+
+public class Staff {
+	
+	private int staffID;
+	private String name;
+	private String email;
+	private String jobTitle;
+	
+	public int getStaffID() {
+		return staffID;
+	}
+	public void setStaffID(int staffID) {
+		this.staffID = staffID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getJobTitle() {
+		return jobTitle;
+	}
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+	
+}
