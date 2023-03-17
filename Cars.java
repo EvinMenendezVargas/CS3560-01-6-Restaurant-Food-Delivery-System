ab8b03e67966977af1d9fcd6da8251beea1b1bd0
jasroop-singh <113493289+jasroop-singh@users.noreply.github.com>
2023-03-17 15:41:12 -0700

    Create Cars.java

---
+++ Cars.java
@@ -0,0 +1,27 @@
+
+public class Cars {
+	
+	private String carModel;
+	private String carColor;
+	private String licensePlateNum;
+	
+	public String getCarModel() {
+		return carModel;
+	}
+	public void setCarModel(String carModel) {
+		this.carModel = carModel;
+	}
+	public String getCarColor() {
+		return carColor;
+	}
+	public void setCarColor(String carColor) {
+		this.carColor = carColor;
+	}
+	public String getLicensePlateNum() {
+		return licensePlateNum;
+	}
+	public void setLicensePlateNum(String licensePlateNum) {
+		this.licensePlateNum = licensePlateNum;
+	}
+
+}
