ff55e3da88d31288fe505a7ad081066e8a361b2b
jasroop-singh <113493289+jasroop-singh@users.noreply.github.com>
2023-03-17 21:48:09 -0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,43 +1,64 @@
-
+// Customer class
 public class Customer {
 	
+	// variables
 	private int customerID;
 	private String customerName;
 	private String customerAddress;
 	private String paymentInfo;
 	private int phoneNumber;
 	
+	// method to get customer ID
 	public int getCustomerID() {
 		return customerID;
 	}
+	
+	// method to set customer ID
 	public void setCustomerID(int customerID) {
 		this.customerID = customerID;
 	}
+	
+	// method to get customer name
 	public String getCustomerName() {
 		return customerName;
 	}
+	
+	// method to set customer name
 	public void setCustomerName(String customerName) {
 		this.customerName = customerName;
 	}
+	
+	// method to get address
 	public String getCustomerAddress() {
 		return customerAddress;
 	}
+	
+	// method to set address
 	public void setCustomerAddress(String customerAddress) {
 		this.customerAddress = customerAddress;
 	}
+	
+	// method to get the payment information
 	public String getPaymentInfo() {
 		return paymentInfo;
 	}
+	
+	// method to set payment information
 	public void setPaymentInfo(String paymentInfo) {
 		this.paymentInfo = paymentInfo;
 	}
+	
+	// method to get phone number
 	public int getPhoneNumber() {
 		return phoneNumber;
 	}
+	
+	// method to set phone number
 	public void setPhoneNumber(int phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 	
+	// method to get all the payment information and then finally pay the order
 	public void makeOrderPayment() {
 		
 		// gets card info and pays for the order
