501bb0be04c3cdaa7e2bb64b403328a728efde2a
NicholasRamirez <98774521+NicholasRamirez@users.noreply.github.com>
2023-03-17 21:51:44 -0700

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,25 +1,37 @@
 
 public class Menu {
 	
+	// variables
 	private String foodName;
 	private int foodItemID;
 	private double foodPrice;
 	
+	// method to get food name
 	public String getFoodName() {
 		return foodName;
 	}
+	
+	// method to set food name
 	public void setFoodName(String foodName) {
 		this.foodName = foodName;
 	}
+	
+	// method to get food item ID
 	public int getFoodItemID() {
 		return foodItemID;
 	}
+	
+	// method to set food item ID
 	public void setFoodItemID(int foodItemID) {
 		this.foodItemID = foodItemID;
 	}
+	
+	// method to get food price
 	public double getFoodPrice() {
 		return foodPrice;
 	}
+	
+	// method to set food price
 	public void setFoodPrice(double foodPrice) {
 		this.foodPrice = foodPrice;
 	}
