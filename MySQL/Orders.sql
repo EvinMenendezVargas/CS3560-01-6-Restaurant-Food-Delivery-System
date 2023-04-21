290dfe50e295c722bade7133dae920395aadb61c
jasroop-singh <113493289+jasroop-singh@users.noreply.github.com>
2023-04-21 14:01:43 -0700

    Update Orders.sql

--- MySQL/Orders.sql
+++ MySQL/Orders.sql
@@ -1,11 +1,16 @@
 USE restaurant;
 
+--create a new table
 CREATE TABLE `orders` (
+  -- enter column names with their data types
   `orderID` int NOT NULL,
   `num_of_items` int NOT NULL,
   `total_price` double NOT NULL,
   `order_status` varchar(45) NOT NULL,
+  
+  -- set orderID as primary key
   PRIMARY KEY (`orderID`)
 )
 
+--example values to test
 INSERT INTO orders VALUES(1, 1, 16.99, 'In Transit');
