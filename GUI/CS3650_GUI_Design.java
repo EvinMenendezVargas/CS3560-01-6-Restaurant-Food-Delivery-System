923aad6205f83f1181b06018f1ccf81748cd3e4d
Evin Menendez Vargas <116537792+UselesssAqua@users.noreply.github.com>
2023-04-21 00:23:30 -0700

    Add files via upload
    Added more features such as add/minus buttons, add order buttons, and a work-in-progress menu bar

--- GUI/CS3650_GUI_Design.java
+++ GUI/CS3650_GUI_Design.java
@@ -6,6 +6,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
@@ -20,8 +21,15 @@ public class CS3650_GUI_Design extends Application
 {
 	public Button orderButton = new Button("Place Order");
 	public Button addButton = new Button("Add to Order");
+	public Button plusButton = new Button("+");
+	public Button minusButton = new Button("-");
+	
+	public TextField countField = new TextField("0");
+	
+	public int value = 0;
 	
 	public Label resTitle = new Label("Welcome to Tony's Ponies!");
+	public Label nameTitle = new Label("Tony's Ponies");
 	public Label menuTitle = new Label("MENU");
 	public Label appetizerTitle = new Label("Appetizers");
 	public Label entreeTitle = new Label("Entrees");
@@ -60,14 +68,37 @@ public class CS3650_GUI_Design extends Application
 			gridpane.setHgap(20);
 			gridpane.setVgap(20);
 			
+			plusButton.setPrefSize(30, 30);
+			minusButton.setPrefSize(30, 30);
+			
+			countField.setPrefSize(30, 30);
+			
+			minusButton.setOnAction(event1 -> {value = Integer.parseInt(countField.getText());
+			if(value <= 0)
+			{
+				countField.setText(Integer.toString(value + 0));
+			}
+			else
+			{
+				countField.setText(Integer.toString(value - 1));
+			}});
+			
+			plusButton.setOnAction(event2 -> {value = Integer.parseInt(countField.getText());
+			countField.setText(Integer.toString(value + 1));});
+			
 			HBox nameBox = new HBox(30, foodName, foodPrice);
-			VBox descriptionBox = new VBox(10, nameBox, foodDesc);
+			HBox addingBox = new HBox(minusButton, countField, plusButton);
+			HBox buttonsBox = new HBox (30, addingBox, addButton);
+			VBox descriptionBox = new VBox(10, nameBox, foodDesc, buttonsBox);
 			HBox foodBox = new HBox(10, view, descriptionBox);
+			
+			HBox menuBar = new HBox(20, nameTitle);
+			menuBar.setAlignment(Pos.TOP_LEFT);
 			
 			gridpane.add(foodBox, 2, 1);
 			gridpane.setAlignment(Pos.CENTER);
 		
-	    	VBox menuBox = new VBox(50, menuTitle, appetizerTitle, gridpane, entreeTitle);
+	    	VBox menuBox = new VBox(25, menuBar, menuTitle, appetizerTitle, gridpane, entreeTitle);
 	    	menuBox.setPadding(new Insets(10));
 	    	menuBox.setAlignment(Pos.TOP_CENTER);
 	    
