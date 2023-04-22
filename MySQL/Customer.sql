cabbf00ed3858271a56ec3ac7d35caca3d8a6e75
jasroop-singh <113493289+jasroop-singh@users.noreply.github.com>
2023-04-21 20:24:13 -0700

    Update Customer.sql

--- MySQL/Customer.sql
+++ MySQL/Customer.sql
@@ -1,7 +1,11 @@
+-- create a new database named restaurant
 CREATE DATABASE restaurant;
 USE restaurant;
 
+-- create a first table
 CREATE TABLE `customer` (
+  
+  -- input all the columns with required datatypes
   `customerID` int NOT NULL,
   `customer_first_name` varchar(45) NOT NULL,
   `customer_last_name` varchar(45) NOT NULL,
@@ -11,7 +15,10 @@ CREATE TABLE `customer` (
   `card_experation_date` varchar(45) NOT NULL,
   `card_ccv` int NOT NULL,
   `phone_number` varchar(45) NOT NULL,
+  
+  -- set customerID as primary key
   PRIMARY KEY (`customerID`)
 )
 
+-- an example value
 INSERT INTO customer VALUES(1, 'Nicholas', 'Ramirez', '1234 Amar Rd', 'Nicholas Ramirez', '1234-5678-1234-5678', '06/23', 123, '626-123-4567');
